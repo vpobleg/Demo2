ca27686647476d961a051a0342ad5d9ec1381f6e
mcontrerass <mcontrerass@gmail.com>
2020-11-10 20:58:22 -0300

    Se agrega feature multiplicacion

--- calculadora.java
+++ calculadora.java
@@ -1,5 +1,6 @@
 public class calculadora{
 	public static void main(String[] args){
 		System.out.println("calculadora:suma");
+	        System.out.println("mcontrerass calculadora:multiplicacion");
 	}
 }
