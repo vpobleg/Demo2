c52876d6756a02373cdb06c1c73d7a729f8dad0d
sebassabes <sebastian.andres.aracena@gmail.com>
2020-11-10 20:45:09 -0300

    calculadora dividir multiplicar y restar

--- calculadora.java
+++ calculadora.java
@@ -1,5 +1,11 @@
 public class calculadora{
 	public static void main(String[] args){
 		System.out.println("calculadora:suma");
+ System.out.println("calculadora:multiplicar");
+  System.out.println("calculadora:dividir");
+System.out.println("calculadora:resta");
+
+
+
 	}
 }
