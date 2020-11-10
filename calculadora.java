e53c3d1c0f0eb24d890c92b09e4a00accc3af7d8
Gerardo Felmer <desarrollo@satelnet.cl>
2020-11-10 20:54:23 -0300

    gfdevops feature

--- calculadora.java
+++ calculadora.java
@@ -4,5 +4,6 @@ public class calculadora{
 		System.out.println("angelo-calc-división");
 		System.out.println("AdrianGomezCL-calc-multiplicación");
     System.out.println("roddyvitali:feature");
+		System.out.println("gfdevopsFeature");
 	}
 }
