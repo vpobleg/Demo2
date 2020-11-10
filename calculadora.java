a5973b9d7e7ff7b7e1621b5d4563f4834813365c
Adrian <adriangomez.cl@gmail.com>
2020-11-10 20:45:40 -0300

    Adrian Gomez - Multiplicacion

--- calculadora.java
+++ calculadora.java
@@ -2,5 +2,6 @@ public class calculadora{
 	public static void main(String[] args){
 		System.out.println("calculadora:suma");
 		System.out.println("angelo-calc-división");
+		System.out.println("AdrianGomezCL-calc-multiplicación");
 	}
 }
