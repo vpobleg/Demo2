b7d20780fadfae82b08551f9eb1056b65ec22186
sebassabes <sebastian.andres.aracena@gmail.com>
2020-11-10 20:48:03 -0300

    calculadora sebasaracena

--- calculadora.java
+++ calculadora.java
@@ -1,9 +1,9 @@
 public class calculadora{
 	public static void main(String[] args){
 		System.out.println("calculadora:suma");
- System.out.println("calculadora:multiplicar");
-  System.out.println("calculadora:dividir");
-System.out.println("calculadora:resta");
+ System.out.println("calculadora seba-aracena:multiplicar");
+  System.out.println("calculadora seba-aracena:dividir");
+System.out.println("calculadora seba-aracena:resta");
 
 
 
