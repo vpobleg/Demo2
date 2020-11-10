10886c03178b58822c3339b844c4c5828a8fce8c
Guillermo Correa <guillermocorreamartinez@gmail.com>
2020-11-10 20:56:35 -0300

    feature-gmocorreamartinez-calc-suma

--- calculadora.java
+++ calculadora.java
@@ -2,5 +2,6 @@ public class calculadora{
 	public static void main(String[] args){
 		System.out.println("calculadora:suma");
 		System.out.println("angelo-calc-división");
+		System.out.println("gmocorreamartinez-calc-suma");
 	}
 }
