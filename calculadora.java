b7b4b1802d074af7ca2abd42afd20592aec158e0
joramdevops <joram.diaz.garrido@gmail.com>
2020-11-10 21:16:07 -0300

    se realizan cambios

--- calculadora.java
+++ calculadora.java
@@ -3,6 +3,10 @@ public class calculadora{
 		System.out.println("calculadora:suma");
 		System.out.println("angelo-calc-división");
 		System.out.println("AdrianGomezCL-calc-multiplicación");
+		System.out.println("joramdevops-calc-suma");
+		System.out.println("joramdevops-calc-resta");
+		System.out.println("joramdevops-calc-multiplicacion");
+		System.out.println("joramdevops-calc-division");
     System.out.println("roddyvitali:feature");
 	}
 }
